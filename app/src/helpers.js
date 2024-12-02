c698d61b805d69285a2313dc6376b3a0d983bafd
Braily Guzman <brailyg@outlook.com>
2024-12-02 13:33:28 -0500

    Add more whitespaces to make the code more readable

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -18,51 +18,65 @@ export const displayPalette = (id, title, colors, temperature) => {
 
     // Palette
     const palette = document.createElement('li');
+
     palette.id = id;
     palette.className = 'palette';
+
     paletteContainer.appendChild(palette);
 
     // Palette -> Title
     const paletteTitle = document.createElement('h3');
+
     paletteTitle.className = 'palette-title';
     paletteTitle.textContent = title;
+
     palette.appendChild(paletteTitle);
 
     // Palette -> Content
     const paletteContent = document.createElement('div');
+
     paletteContent.className = 'palette-content';
+
     palette.appendChild(paletteContent);
 
     // Palette -> Content -> Color Container
     const paletteColorContainer = document.createElement('div');
+
     paletteColorContainer.className = 'palette-color-container';
+
     paletteContent.appendChild(paletteColorContainer);
 
     // Displaying Each Color
     colors.forEach((color) => {
         // Palette -> Content -> Color Container -> Palette Color
         const paletteColor = document.createElement('div');
+
         paletteColor.className = 'palette-color';
         paletteColorContainer.appendChild(paletteColor);
 
         // Palette -> Content -> Color Container -> Palette Color -> Box
         const paletteColorBox = document.createElement('div');
+
         paletteColorBox.className = 'palette-color-box';
         paletteColorBox.style.backgroundColor = color;
+
         // Palette -> Content -> Color Container -> Palette Color -> Box -> Span
         paletteColorBox.innerHTML = `
             <span class="palette-color-text">Text</span>
             <span class="palette-color-text">Example</span>
         `;
+
         paletteColor.appendChild(paletteColorBox);
 
         // Palette -> Content -> Color Container -> Palette Color -> Copy
         const copyButton = document.createElement('button');
+
         copyButton.className = 'palette-color-copy';
         copyButton.textContent = `Copy ${color}`;
 
         copyButton.addEventListener('click', () => {
             navigator.clipboard.writeText(color);
+
             const oldTextContent = copyButton.textContent;
             copyButton.textContent = 'Copied hex!';
 
@@ -83,13 +97,16 @@ export const displayPalette = (id, title, colors, temperature) => {
     deletePalette.addEventListener('click', () => {
         deletePaletteHandler(id);
     });
+
     paletteContent.appendChild(deletePalette);
 
     // Palette -> Temperature
     const paletteTemperature = document.createElement('p');
+
     paletteTemperature.className = 'palette-temperature';
     paletteTemperature.textContent = temperature;
     paletteTemperature.style.backgroundColor = temperatures[temperature];
+
     palette.appendChild(paletteTemperature);
 };
 
