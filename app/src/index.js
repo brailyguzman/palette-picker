bc9b48a9ebba235db93d33d643b836b2ef22058f
Braily Guzman <brailyg@outlook.com>
2024-12-02 13:25:23 -0500

    Implement initial functionality

--- app/src/index.js
+++ app/src/index.js
@@ -1,1 +1,15 @@
+import { displayPalette, handleSubmit } from './helpers.js';
+import { initPalettes } from './storage.js';
 import './style.css';
+
+document
+    .getElementById('add-palette-form')
+    .addEventListener('submit', handleSubmit);
+
+const palettes = initPalettes();
+
+for (const palette in palettes) {
+    const { colors, temperature, title, uuid: id } = palettes[palette];
+
+    displayPalette(id, title, colors, temperature);
+}
